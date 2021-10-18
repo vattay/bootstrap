65dfaf011c87e8f01caba0cd3bf439cd21394ac3
Anton Vattay <anton@v4x.io>
2021-10-18 11:06:38 -0400

    Actual wine install sh.

---
+++ install-wine.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo dpkg --add-architecture i386 
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo apt-key add winehq.key
+sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ hirsute main'
+
