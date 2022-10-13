ef7e03d9d03a576ba18ee6500b0aa1b4d83b0519
Anton Vattay <anton@cuttingedgeai.com>
2022-10-13 15:58:42 -0400

    Update docker install.

--- install-docker.sh
+++ install-docker.sh
@@ -3,21 +3,21 @@
 sudo apt-get update
 
 sudo apt-get install \
-    apt-transport-https \
     ca-certificates \
     curl \
     gnupg \
     lsb-release
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 # Works on distributions like PopOS but not Ubuntu, see install-nvidia-docker.sh
 # nvidia-container-toolkit
